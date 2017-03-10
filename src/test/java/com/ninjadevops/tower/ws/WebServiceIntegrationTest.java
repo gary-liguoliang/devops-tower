804e45a4362606b49bbb176a6444d3c62cc15f5e
guoliang-dev <guoliang.me@gmail.com>
2017-03-10 22:31:31 +0800

    added web service integration test.

--- src/test/java/com/ninjadevops/tower/ws/WebServiceIntegrationTest.java
+++ src/test/java/com/ninjadevops/tower/ws/WebServiceIntegrationTest.java
@@ -4,14 +4,12 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by me@liguoliang.com on 3/10/2017.
@@ -28,6 +26,15 @@ public class WebServiceIntegrationTest {
     @Test
     public void getStatus() throws Exception {
         String wsStatus = restTemplate.getForObject("http://localhost:" + port + "/status", String.class);
+
         assertThat(wsStatus, CoreMatchers.containsString("DevOps Tower is running"));
+    }
+
+    @Test
+    public void testSqlExec() throws Exception {
+        String url = "http://localhost:" + port + "/sql-exec?conn=db-sit2&sql-job-id=get-env-name";
+        String resultString = restTemplate.getForObject(url, String.class);
+
+        assertThat(resultString, CoreMatchers.containsString("SIT"));
     }
 }
